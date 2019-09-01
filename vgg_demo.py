c44801e23f66511cf93e930152d605d4d496dec2
chaudharyachint08 <chaudharyachint08@gmail.com>
2019-09-01 12:28:40 +0530

    Few changes

--- vgg_demo.py
+++ vgg_demo.py
@@ -109,13 +109,13 @@ image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 image = preprocess_input(image)
 
 # predict the probability across all output classes
-# yhat = model.predict(image)
+yhat = model.predict(image)
 
 # convert the probabilities to class labels
-# label = decode_predictions(yhat)
+label = decode_predictions(yhat)
 
 # retrieve the most likely result, e.g. highest probability
-# label = label[0][0]
+label = label[0][0]
 
 # print the classification
-# print('%s (%.2f%%)' % (label[1], label[2]*100))
+print('%s (%.2f%%)' % (label[1], label[2]*100))
